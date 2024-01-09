445988d8b2c17a96cfcb94b5e0e2f27dd43f7d9e
fabon <syobon.hinata.public@gmail.com>
2024-01-10 00:54:14 +0900

    Add French Republican Date

--- src/calendars/FrenchRepublicanCalendar.tsx
+++ src/calendars/FrenchRepublicanCalendar.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { Temporal } from 'temporal-polyfill'
 import { Text, VStack } from '@kuma-ui/core'
-import { padNum } from '../utils'
+import { autumnalEquinox, padNum, toRoman } from '../utils'
 
 interface Props {
   date: Temporal.ZonedDateTime
@@ -25,13 +25,78 @@ function convertTime(time: Temporal.PlainTime) {
   }
 }
 
-function toDateTime(date: Temporal.ZonedDateTime, isParis: boolean) {
-  if (isParis) {
-    // until 1911 Paris Mean Time is 560.935 seconds ahead of Greenwich Mean Time
-    return date
-      .withTimeZone('UTC')
+function lastAutumnalEquinoxInParis(date: Temporal.PlainDate) {
+  const equinoxDate = convertToLegacyParisTime(
+    autumnalEquinox(date.year),
+  ).toPlainDate()
+  if (Temporal.PlainDate.compare(equinoxDate, date) === 1) {
+    // Last year
+    return convertToLegacyParisTime(
+      autumnalEquinox(date.year - 1),
+    ).toPlainDate()
+  } else {
+    return equinoxDate
+  }
+}
+
+function convertDate(date: Temporal.PlainDate) {
+  const equinoxDate = lastAutumnalEquinoxInParis(date)
+  const year = toRoman(equinoxDate.year - 1791)
+  const days = equinoxDate.until(date).days
+  if (days < 360) {
+    const month = [
+      'Vendémiaire',
+      'Brumaire',
+      'Frimaire',
+      'Nivôse',
+      'Pluviôse',
+      'Ventôse',
+      'Germinal',
+      'Floréal',
+      'Prairial',
+      'Messidor',
+      'Thermidor',
+      'Fructidor',
+    ][Math.floor(days / 30)]
+    const day = (days % 30) + 1
+    return `${day} ${month} an ${year}`
+  }
+  const dayName = [
+    'La Fête de la Vertu',
+    'La Fête du Génie',
+    'La Fête du Travail',
+    "La Fête de l'Opinion",
+    'La Fête des Récompenses',
+    'La Fête de la Révolution',
+  ][days - 360]
+  if (dayName) {
+    throw new Error('Assertion')
+  }
+  return `${dayName}, an ${year}`
+}
+
+function convertToLegacyParisTime(
+  date: Temporal.ZonedDateTime | Temporal.Instant,
+): Temporal.PlainDateTime {
+  const utcDate =
+    date instanceof Temporal.ZonedDateTime
+      ? date.withTimeZone('UTC')
+      : date.toZonedDateTimeISO('UTC')
+
+  return (
+    utcDate
+      // until 1911 Paris Mean Time is 560.935 seconds ahead of Greenwich Mean Time
       .add(Temporal.Duration.from({ seconds: 560, milliseconds: 935 }))
       .toPlainDateTime()
+  )
+}
+
+function toDateTime(
+  date: Temporal.ZonedDateTime,
+  isParis: boolean,
+): Temporal.PlainDateTime {
+  if (isParis) {
+    return convertToLegacyParisTime(date)
   }
   return date.toPlainDateTime()
 }
@@ -39,10 +104,11 @@ function toDateTime(date: Temporal.ZonedDateTime, isParis: boolean) {
 function convert(date: Temporal.ZonedDateTime, isParis: boolean) {
   const dateTime = toDateTime(date, isParis)
   const republicanTime = convertTime(dateTime.toPlainTime())
-  return `${republicanTime.hour} ${padNum(republicanTime.minute, 2)} ${padNum(
-    republicanTime.second,
+  const republicanDate = convertDate(dateTime.toPlainDate())
+  return `${republicanDate} ${republicanTime.hour} ${padNum(
+    republicanTime.minute,
     2,
-  )}`
+  )} ${padNum(republicanTime.second, 2)}`
 }
 
 export default function FrenchRepublicanCalendar({ date }: Props) {
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,35 @@
+import { Temporal } from 'temporal-polyfill'
+
 export function padNum(num: number, maxDigit: number) {
   return num.toString().padStart(maxDigit, '0')
+}
+
+export function toRoman(num: number) {
+  if (num >= 4000) {
+    throw new Error(`Number too large: ${num}`)
+  }
+  const d1000 = Math.floor(num / 1000)
+  const d100 = Math.floor((num % 1000) / 100)
+  const d10 = Math.floor((num % 100) / 10)
+  const d1 = num % 10
+  const r1000 = ['', 'M', 'MM', 'MMM']
+  const r100 = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
+  const r10 = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
+  const r1 = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  return r1000[d1000]! + r100[d100]! + r10[d10]! + r1[d1]!
+}
+
+export function vernalEquinox(year: number) {
+  // https://astronomy.stackexchange.com/questions/43283/accuracy-of-calculating-the-vernal-equinox
+  // days from 2000-01-01T00:00:00Z
+  const days = 79.313 + 365.2422 * (year - 2000)
+  const seconds = Math.floor(days * 24 * 60 * 60)
+  return Temporal.Instant.from('2000-01-01T00:00:00Z').add({ seconds })
+}
+
+export function autumnalEquinox(year: number) {
+  const days = 265.712 + 365.2422 * (year - 2000)
+  const seconds = Math.floor(days * 24 * 60 * 60)
+  return Temporal.Instant.from('2000-01-01T00:00:00Z').add({ seconds })
 }
